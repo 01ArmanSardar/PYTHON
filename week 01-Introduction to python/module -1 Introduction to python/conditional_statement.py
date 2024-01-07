dce327be0049bc6460ee4c696d569879ed772862
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-07 21:52:13 +0600

    pitorn practice

--- week 01-Introduction to python/module -1 Introduction to python/conditional_statement.py
+++ week 01-Introduction to python/module -1 Introduction to python/conditional_statement.py
@@ -1,27 +1,28 @@
 # in ,not ,not in ,is ,is not 
-
-""" a=2
+#logical operator c teh amrah    || &&    lektahm ,kintu PYTHON a amrah    or and    likbho
+a=2
 if a>5:
     print('5 er beshi')
 elif a==2:#python a else if keh short-from koreh   elif   likha hoi
     print('2 er soman')
 else:
-    print('5 er kom') """
-
+    print('5 er kom') 
+#....................
 boss =False
-""" if boss is True:
+if boss is True: #is True die True bujai
     print('boss keh thel diteh jacchi')
 else :
-    print('akhn time nai') """
-""" if boss is not True:
+    print('akhn time nai') 
+
+if boss is not True: #is not True dieo kintu False bujai
     print('akhn time nai')
 else :
-    print('boss keh thel diteh jacchi') """
+    print('boss keh thel diteh jacchi') 
 
 #nasted conditons 
 coin ='head'
 if boss==False:
-    print('boss you are joos')
+    print('boos you are joos')
     if coin=='tail':
         print('batting')
     else:
---
+++ week 01-Introduction to python/module -1 Introduction to python/while_loop.py
@@ -0,0 +1,20 @@
+#normal while loop
+""" num=1
+while num<=10:
+    print(num)
+    num+=1 """
+#while loop with break continue statement
+""" num=1
+while num<=10:
+    print(num)
+    num+=1
+    if num==6:
+        break
+     """
+num=1
+while num<=10:
+    num+=1
+    if num%2==1:
+        continue
+    print(num)
+    
