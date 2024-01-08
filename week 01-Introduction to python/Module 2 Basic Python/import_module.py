20885ba77b2b29413e3bf44c36c81d942d0d1be5
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-08 22:15:08 +0600

    pitron practice

--- week 01-Introduction to python/Module 2 Basic Python/import_module.py
+++ week 01-Introduction to python/Module 2 Basic Python/import_module.py
@@ -1,5 +1,6 @@
-""" from function import double_it # explanation this line : from   aktah_file_er_name   import   oi_file_er_aktah_function_er_name
-double_it(20) """
-from kargs_multiple import full_name as name
+from function import double_it # explanation this line : from   aktah_file_er_name   import   oi_file_er_aktah_function_er_name
+double_it(20)
+from kargs_multiple import full_name as name # ai line extra jeh  as  name lekchi ,tar karon holo amra full_name function tar aktu choto koreh   name  raklam ar kih
+from default_args import * # ai line tar mane hocceh ami  default_args  name er aktah file tekeh shob gulu function ai file a nie anlam,
 return_output=name('kacha ','morich')
 print(return_output)
---
+++ week 01-Introduction to python/Module 2 Basic Python/list_lisTindex_fun_and_slice.py
@@ -0,0 +1,19 @@
+#index=   0  1   2   3   4   5   6   7   this indexing is forward and positive
+numbers=[45, 56, 23, 62, 89, 54, 12, 67]
+#index =  -8  -7  -6 -5  -4  -3  -2  -1  this indexing is bakward and negative 
+
+print(numbers[3],numbers[-3]) #akhne 62 abong 54 duitai print hobhe.
+
+#list[start_index : end_index] #below line print the list,    start from the start index and stops before the end index
+print(numbers[2:5])
+
+#list[start_index :end_index :step] #step manhe koitah index por por print korbho
+print(numbers[1:7:1])
+print(numbers[1:7:2])
+print(numbers[7:2:-1])#maneh 7 index tekeh 2 index er dikeh jabho
+
+print(numbers[:6])#akhne amra sudhu end index dichi start index dei nai kintu python by default start index prothom tekeh dhore niceh
+print(numbers[1:])#akhne amra sudhu start index dichi end index dei nai kintu python by default end index sesh projontho dhore niceh
+print(numbers[:])#jodhi start and end index kono tai nah dei tahole puro list tai print hoie jabhe
+
+print(numbers[::-1])
