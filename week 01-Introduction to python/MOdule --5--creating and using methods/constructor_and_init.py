07c558b8a5c812e5870098db7c576aed03b5b9ac
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-14 18:56:47 +0600

    pitron practice

--- week 01-Introduction to python/MOdule --5--creating and using methods/constructor_and_init.py
+++ week 01-Introduction to python/MOdule --5--creating and using methods/constructor_and_init.py
@@ -1,4 +1,4 @@
-""" class Phone:
+class Phone:
     def __init__(self,owner,brand,price):
         self.owner=owner
         self.brand=brand
@@ -15,11 +15,11 @@ herphone=Phone('minara','samsung',6700)
 print(herphone.owner,herphone.brand,herphone.price)
 
 dadphone=Phone('abbu','symphony',7000)
-print(dadphone.owner,dadphone.brand,dadphone.price) """
+print(dadphone.owner,dadphone.brand,dadphone.price)
 
 # pen homework
 
-class pen:
+""" class pen:
     def __init__(self,owner,color,price):
         self.owner=owner
         self.color=color
@@ -29,6 +29,6 @@ print(janntpen.owner,janntpen.color,janntpen.price)
 mypen=pen('arman','blue',10)
 print(mypen.owner,mypen.color,mypen.price)
 marjupen=pen('marjan','black',15)
-print(marjupen.owner,marjupen.color,marjupen.price)
+print(marjupen.owner,marjupen.color,marjupen.price) """
 
         
