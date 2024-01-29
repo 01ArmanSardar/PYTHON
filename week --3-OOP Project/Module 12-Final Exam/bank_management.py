5a0f01fdc486f9e1a7cd10f65b41b08f4d09302f
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-29 11:17:32 +0600

    pitron practice

--- week --3-OOP Project/Module 12-Final Exam/bank_management.py
+++ week --3-OOP Project/Module 12-Final Exam/bank_management.py
@@ -23,7 +23,7 @@ class Bank:
     def total_loans(self):
         print(self.total_loan)
 
-    def loan_status_(self, tru_fls):
+    def loan_status_(self, tru_fls):#ok
         print('line 27 ',tru_fls)
         self.loan_status = tru_fls
         print(self.loan_status)
@@ -61,12 +61,15 @@ class Account(Bank):
             print("\n withdrawal amount exceeded")
 
     def check_avilable_balance(self):
-        print(self.balance)
+        print(f'your avilable balance : {self.balance}')
 
     def tranesfer_money(self, another_accNo, amount):
         if amount <= self.balance:
-            self.balance -= amount
-            another_accNo.balance += amount
+            for accounnt in self.accounts:
+                if accounnt.accountNo==another_accNo:
+                    accounnt.balance+=amount
+                    self.balance -= amount
+                    break
         else:
             print('Account does not exist')
 
@@ -78,6 +81,7 @@ class Account(Bank):
         if self.loan_limit > 0:
             self.balance += amount
             self.loan_limit -= 1
+            Bank.total_loan+=amount
         else:
             print('your loan limit exceeded')
 
@@ -147,8 +151,11 @@ while True:
                 amount = int(input('Type deposit amount:'))
                 currentUser.deposit(amount)
             elif op == 6:
-                amount = int(input('Type withdrawal amount:'))
-                currentUser.withdraw(amount)
+                if bank.bankrupt==False:
+                    amount = int(input('Type withdrawal amount:'))
+                    currentUser.withdraw(amount)
+                else:
+                    print('Bank is bankrupt')
             elif op == 7:
                 currentUser = None
             else:
@@ -163,18 +170,20 @@ while True:
             print('7. Logout\n')
 
             op = int(input("Choose Option:"))
+            #option is uncomplete
             if op == 1:
                 amount = int(input("Enter withdrawal amount:"))
                 currentUser.withdraw(amount)
+
             elif op == 2:
                 delete_account_no = input("Enter deletable account no:")
-                currentUser.delete_account(delete_account_no)
+                bank.delete_account(delete_account_no)
             elif op == 3:
-                currentUser.show_all_user_acount()
+                bank.show_all_user_acount()
             elif op == 4:
-                currentUser.total_balances()
+                bank.total_balances()
             elif op == 5:
-                currentUser.total_loans()
+                bank.total_loans()
             elif op == 6:
                 tru_flss = bool(input('Type True or False for loan status'))
                 bank.loan_status_(tru_flss)
